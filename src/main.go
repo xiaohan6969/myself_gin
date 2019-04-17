910c1f2e14bb44f0a7014994ba6225b20f848d5f
xiaohan6969 <948309461@qq.com>
2019-04-17 13:42:58 +0800

    第二次提交，添加了viper。

--- src/main.go
+++ src/main.go
@@ -1,11 +1,22 @@
 package main
 
 import (
+	"config"
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
+	"net/http"
 	"routers"
 )
 
 func main() {
+	if err := config.Init(""); err != nil {
+		panic(err)
+	}
+	gin.SetMode(viper.GetString("runmode"))
 	g := gin.Default()
 	routers.Load(g)
+	err := http.ListenAndServe(viper.GetString("port"), g)
+	if err != nil {
+		//logging.Error(err.Error())
+	}
 }
--- src/routers/router.go
+++ src/routers/router.go
@@ -1,22 +1,18 @@
 package routers
 
 import (
+	"controllers/ReadExcel"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	"controllers/ReadExcel"
 )
 
 func Load(g *gin.Engine) *gin.Engine {
 	g.HEAD("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-		})
+		c.JSON(http.StatusOK, gin.H{})
 	})
-	//文件导入发货(批量)
-	g.POST("/batch/delivery", ReadExcel.ReadExcel)
-
-
-
-
+	//文件导入(批量)
+	g.POST("/pf/operation/excel", ReadExcel.ReadExcel)
+	g.GET("/pf/practice", ReadExcel.ReadExcel)
 
 	g.NoRoute(func(c *gin.Context) {
 		c.String(http.StatusNotFound, "The incorrect API route.")
